ff5e0d6b2e84fbd58b08f88ef67bf83745cad3d7
Valzionn <144949469+Valzionn@users.noreply.github.com>
2024-04-11 19:18:04 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import TeamClock from './TeamClock'
+import TeamObject from './TeamObject'
+import GameReviews from './GameReviews'
+import ShoppingList from './ShoppingList'
+
+export default function Home () {
+  return (
+    <>
+      {/* <TeamClock /> */}
+      {/* <TeamObject /> */}
+      {/* <GameReviews /> */}
+      <ShoppingList />
+    </>
+  )
+}
